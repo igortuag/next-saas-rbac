ba705e68d69a4d463b506193e65071c97de7007e
Igor Tuag <igortuag@gmail.com>
2026-02-07 08:32:23 -0300

    feat: implement ability management with CASL in auth module

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,5 +1,22 @@
-const test = "Hello world!";
+import { AbilityBuilder, createMongoAbility, type CreateAbility, type ForcedSubject, type MongoAbility } from '@casl/ability'
 
-export function greet() {
-  console.log(test);
-}
+const actions = ['manage', 'create', 'read', 'update', 'delete', 'invite'] as const
+const subjects = ['all', 'User'] as const
+
+type AppAbilities = [
+  typeof actions[number],
+  (
+    | typeof subjects[number]
+    | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
+  )
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility
+
+const { build, can, cannot } = new AbilityBuilder<AppAbility>(createAppAbility)
+
+can('invite', 'User')
+cannot('invite', 'User')
+
+export const ability = build()
