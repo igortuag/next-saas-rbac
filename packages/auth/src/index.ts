45d34ed06e1920a4062aae03d52f038c98ddc6cf
Igor Tuag <igortuag@gmail.com>
2026-02-11 09:37:51 -0300

    feat: enhance ability management by defining user roles and permissions

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,6 @@
 import { AbilityBuilder, createMongoAbility, type CreateAbility, type ForcedSubject, type MongoAbility } from '@casl/ability'
+import type { User } from './models/user'
+import { permissions } from './permissions'
 
 const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', "all"] as const
@@ -14,9 +16,17 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility
 
-const { build, can, cannot } = new AbilityBuilder<AppAbility>(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
 
-export const ability = build()
+export function defineAbilitiesFor(user: User) {
+  const builder = new AbilityBuilder<AppAbility>(createAppAbility)
+
+  if (typeof permissions[user.role] !== "function") {
+    throw new Error(`Permissions for role ${user.role} are not defined`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+  return ability
+}
