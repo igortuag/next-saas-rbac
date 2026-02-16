99a9e214bbc1a72be3c8532625754f53c3cf83e2
Igor Tuag <igortuag@gmail.com>
2026-02-16 08:40:28 -0300

    feat: redefine ProjectSubject using zod schema for improved validation

--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,3 +1,8 @@
-export type ProjectSubject = [
-  'create' | 'delete' | 'manage', 'Project'
-]
+import { z } from 'zod'
+
+export const ProjectSubjectSchema = z.tuple([
+  z.union([z.literal('manage'), z.literal('get'), z.literal('create'), z.literal('update'), z.literal('delete')]),
+  z.literal('Project'),
+])
+
+export type ProjectSubject = z.infer<typeof ProjectSubjectSchema>
