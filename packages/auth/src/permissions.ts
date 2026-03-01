0efe8bc24242d31b7b1d38621c2a0450aa2fa3da
Igor Tuag <igortuag@gmail.com>
2026-03-01 08:12:16 -0300

    feat: update ADMIN permissions to include transfer ownership and update for organizations

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -9,18 +9,12 @@ type PermissionsByRole = (
 ) => void;
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all');
+
+    cannot(['transfer_ownership', 'update'], 'Organization');
+    can(['transfer_ownership', 'update'], 'Organization', { ownerId: user.id });
   },
-  MEMBER(user, { can }) {
-    can('invite', 'User');
-    can('manage', 'Project');
-    can('delete', 'Organization', {
-      ownerId: { $eq: user.id },
-    });
-    can(['update', 'delete'], 'Project', {
-      ownerId: { $eq: user.id },
-    });
-  },
-  BILLING() {},
+  MEMBER(user, { can }) {},
+  BILLING(_, { can }) {},
 };
