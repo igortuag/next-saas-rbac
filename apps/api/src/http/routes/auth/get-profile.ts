2ddc2eeb550c868fad6dad4f341f0a3c25fb9991
Igor Tuag <igortuag@gmail.com>
2026-04-13 08:25:57 -0300

    fix: correct response schema definition in get profile route

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -1,15 +1,25 @@
 import { prisma } from '@/lib/prisma';
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
 
-export async function authenticateWithPassword(app: FastifyInstance) {
+export async function getProfile(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
     '/profile',
     {
       schema: {
         tags: ['auth'],
         summary: 'Get the authenticated user profile',
-        reponse: {},
+        reponse: {
+          200: z.object({
+            user: z.object({
+              id: z.string(),
+              email: z.string(),
+              name: z.string().nullable(),
+              avatarUrl: z.string().nullable(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
