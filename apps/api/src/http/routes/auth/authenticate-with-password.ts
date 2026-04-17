7fa59c550f1700c081be4ac428da6cf485fb5787
Igor Tuag <igortuag@gmail.com>
2026-04-17 08:00:35 -0300

    fix: streamline error handling in authenticateWithPassword route

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import z from 'zod';
+import { BadRequestError } from '../_errors/bad-request-error';
 
 export async function authenticateWithPassword(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -16,9 +17,6 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           password: z.string(),
         }),
         reponse: {
-          400: z.object({
-            message: z.string(),
-          }),
           201: z.object({
             token: z.string(),
           }),
@@ -35,16 +33,11 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       });
 
       if (!userFromEamil) {
-        return reply.status(400).send({
-          message: 'Invalid email or password',
-        });
+        throw new BadRequestError('Invalid email or password');
       }
 
       if (userFromEamil.passwordHash === null) {
-        return reply.status(400).send({
-          message:
-            'User does not have a password set, please use another authentication method',
-        });
+        throw new BadRequestError('Invalid email or password');
       }
 
       const isPasswordValid = await compare(
@@ -53,9 +46,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       );
 
       if (!isPasswordValid) {
-        return reply.status(400).send({
-          message: 'Invalid email or password',
-        });
+        throw new BadRequestError('Invalid email or password');
       }
 
       const token = await reply.jwtSign(
