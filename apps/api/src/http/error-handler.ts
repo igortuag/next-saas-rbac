bcf7cc2c9ef800356c7eff68a25a8fbe8cdb48f3
Igor Tuag <igortuag@gmail.com>
2026-04-19 07:52:42 -0300

    feat: integrate custom error handler into Fastify server

--- apps/api/src/http/error-handler.ts
+++ apps/api/src/http/error-handler.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { ZodError } from 'zod';
+import { BadRequestError } from './routes/_errors/bad-request-error';
+import { UnauthorizedError } from './routes/_errors/unauthorized-error copy';
 
 type FastifyErrorHandler = FastifyInstance['errorHandler'];
 
@@ -14,4 +16,20 @@ export const errorHandler: FastifyErrorHandler = async (
       issues: error.issues,
     });
   }
+
+  if (error instanceof BadRequestError) {
+    return reply.status(400).send({
+      message: error.message,
+    });
+  }
+
+  if (error instanceof UnauthorizedError) {
+    return reply.status(401).send({
+      message: error.message,
+    });
+  }
+
+  return reply.status(500).send({
+    message: 'Internal Server Error',
+  });
 };
