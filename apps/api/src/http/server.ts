037018a04b498f380b6c358509ffc77a85b99d98
Igor Tuag <igortuag@gmail.com>
2026-05-12 08:09:15 -0300

    feat: integrate environment variables for server port and JWT secret

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -16,6 +16,7 @@ import { getProfile } from './routes/auth/get-profile';
 import { errorHandler } from './error-handler';
 import { requestPasswordRecover } from './routes/auth/request-password-recover';
 import { resetPassword } from './routes/auth/reset-password';
+import { env } from '@saas/env';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -43,7 +44,7 @@ app.register(fastifySwaggerUI, {
 app.register(fastifyCors);
 
 app.register(fastifyJwt, {
-  secret: 'supersecretkey',
+  secret: env.JWT_SECRET,
 });
 
 app.register(createAccount);
@@ -52,6 +53,6 @@ app.register(getProfile);
 app.register(requestPasswordRecover);
 app.register(resetPassword);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running on port 3333');
+app.listen({ port: env.SERVER_PORT }).then(() => {
+  console.log(`Server is running on port ${env.SERVER_PORT}`);
 });
--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
+    SERVER_PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
     GITHUB_OAUTH_CLIENT_ID: z.string(),
@@ -14,6 +15,7 @@ export const env = createEnv({
   },
   shared: {},
   runtimeEnv: {
+    SERVER_PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
