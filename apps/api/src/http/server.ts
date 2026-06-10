e6dedafa16cc30e675f0cc994eced7f9b689c878
Igor Tuag <igortuag@gmail.com>
2026-06-10 08:18:10 -0300

    feat: register createProject route in server

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getOrganizations } from './routes/orgs/get-organizations';
 import { updateOrganization } from './routes/orgs/update-organization';
 import { shutdownOrganization } from './routes/orgs/shotdown-organization';
 import { transferOrganization } from './routes/orgs/transfer-organization';
+import { createProject } from './routes/projects/create-project';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -73,6 +74,8 @@ app.register(getOrganizations);
 app.register(updateOrganization);
 app.register(shutdownOrganization);
 app.register(transferOrganization);
+
+app.register(createProject);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on port ${env.SERVER_PORT}`);
