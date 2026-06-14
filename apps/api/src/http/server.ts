587bbb64768da2ea20b37dedbea6f48736c8bcff
Igor Tuag <igortuag@gmail.com>
2026-06-14 08:23:02 -0300

    feat: register deleteProject route for removing projects from an organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -24,6 +24,7 @@ import { updateOrganization } from './routes/orgs/update-organization';
 import { shutdownOrganization } from './routes/orgs/shotdown-organization';
 import { transferOrganization } from './routes/orgs/transfer-organization';
 import { createProject } from './routes/projects/create-project';
+import { deleteProject } from './routes/projects/delete-project';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -76,6 +77,7 @@ app.register(shutdownOrganization);
 app.register(transferOrganization);
 
 app.register(createProject);
+app.register(deleteProject);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on port ${env.SERVER_PORT}`);
