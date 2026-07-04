98c9711a2be6f33db437377acc1f7a7fc11be3e5
Igor Tuag <igortuag@gmail.com>
2026-07-04 06:42:45 -0300

    feat: register getInvites route to handle invites retrieval for an organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -33,6 +33,7 @@ import { updateMember } from './routes/members/update-member';
 import { removeMember } from './routes/members/remove-member';
 import { createInvite } from './routes/invites/create-invite';
 import { getInvite } from './routes/invites/get-invite';
+import { getInvites } from './routes/invites/get-invites';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -96,6 +97,7 @@ app.register(removeMember);
 
 app.register(createInvite);
 app.register(getInvite);
+app.register(getInvites);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on port ${env.SERVER_PORT}`);
