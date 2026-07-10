3452b1d4178e205dd1ce0746e3ab1003918a3797
Igor Tuag <igortuag@gmail.com>
2026-07-10 08:02:14 -0300

    feat: add getPendingInvites route to retrieve pending invites for the current user

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -37,6 +37,7 @@ import { getInvites } from './routes/invites/get-invites';
 import { revokeInvite } from './routes/invites/revoke-invite';
 import { acceptInvite } from './routes/invites/accept-invite';
 import { rejectInvite } from './routes/invites/reject-invite';
+import { getPendingInvites } from './routes/invites/get-pending-invites';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -104,6 +105,7 @@ app.register(getInvites);
 app.register(acceptInvite);
 app.register(rejectInvite);
 app.register(revokeInvite);
+app.register(getPendingInvites);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on port ${env.SERVER_PORT}`);
