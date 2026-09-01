a5a596c2cd15358df7f58d959c11f5e917aeb3c4
Igor Tuag <igortuag@gmail.com>
2026-09-01 08:26:56 -0300

    feat: implement GitHub sign-in functionality with redirect

--- apps/web/app/auth/actions.ts
+++ apps/web/app/auth/actions.ts
@@ -1,3 +1,11 @@
 'use server';
 
-export function signInWithGithub() {}
+import { redirect } from 'next/navigation';
+
+export function signInWithGithub() {
+  const githubSignInURL = new URL('https://github.com/login/oauth/authorize');
+  githubSignInURL.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID!);
+  githubSignInURL.searchParams.set('scope', 'read:user user:email');
+
+  redirect(githubSignInURL.toString());
+}
