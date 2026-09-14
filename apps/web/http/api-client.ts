fcffab9d326405a99cc1d50087529cd83bc9f4c0
Igor Tuag <igortuag@gmail.com>
2026-09-14 08:41:10 -0300

    feat: update api client to use dynamic token retrieval for Authorization header

--- apps/web/http/api-client.ts
+++ apps/web/http/api-client.ts
@@ -1,14 +1,22 @@
+import { getCookie } from 'cookies-next';
 import ky from 'ky';
 
 export const api = ky.create({
   prefix: process.env.NEXT_PUBLIC_API_URL,
   hooks: {
     beforeRequest: [
-      ({ request }) => {
-        request.headers.set(
-          'Authorization',
-          `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-        );
+      async ({ request }) => {
+        const { cookies: serverCookies } = await import('next/headers');
+
+        const token = getCookie('token', { cookies: serverCookies });
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        } else {
+          const token = getCookie('token');
+          if (token) {
+            request.headers.set('Authorization', `Bearer ${token}`);
+          }
+        }
       },
     ],
   },
