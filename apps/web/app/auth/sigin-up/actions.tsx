208ec47f24170fbaea6df43d0f2ee99d5d37fa7c
Igor Tuag <igortuag@gmail.com>
2026-09-24 16:02:09 -0300

    feat: add sign-up functionality with validation and API integration

---
+++ apps/web/app/auth/sigin-up/actions.tsx
@@ -0,0 +1,61 @@
+'use server';
+
+import { success, z } from 'zod';
+
+import { SignInWithPasswordRequest } from '@/http/sign-in-with-password';
+import { HTTPError } from 'ky';
+import { cookies } from 'next/headers';
+
+const signUpSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
+});
+
+export async function signUpWithEmailAndPassword(data: FormData) {
+  const result = signUpSchema.safeParse(Object.fromEntries(data));
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors;
+
+    return {
+      success: false,
+      message: null,
+      errors,
+    };
+  }
+
+  const { email, password } = result.data;
+
+  try {
+    const { token } = await signUpWithPassword({
+      email: email,
+      password: password,
+    });
+
+    (await cookies()).set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    });
+
+    return {
+      success: true,
+      message: null,
+      errors: null,
+    };
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json();
+      return {
+        success: false,
+        message,
+        errors: null,
+      };
+    }
+    return {
+      success: false,
+      message: 'An unexpected error occurred. Please try again later.',
+      errors: null,
+    };
+  }
+}
